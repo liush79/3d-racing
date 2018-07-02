de4468cdf445c58516455689f4bd87ceb91031b4
seungho.ryu <98koom@hanmail.net>
2018-07-02 22:16:51 +0900

    First commit.

---
+++ Assets/Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarControlActive : MonoBehaviour {
+
+    public GameObject CarControl;
+
+    void Start () {
+        CarControl.GetComponent<CarUserControl>().enabled = true;
+    }	
+}
